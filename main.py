50dfba5e6865c1a1e1a6c4b5203bdbd377a8b4c9
Lentenamen <c24383676@mytudublin.ie>
2026-09-05 22:51:56 +0100

    Refactor CSV reading and rename columns
    Refactored code to use a function for reading CSV and added column renaming.

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv(
-    "data/raw/PPR-ALL.csv",
-    encoding="cp1252"
-)
+"""
+read csv to dataframe
+cp1252 used for the € sign
+"""
+def read_csv():
+    df = pd.read_csv(
+        "data/raw/PPR-ALL.csv",
+        encoding="cp1252",
+        quotechar='"',
+        low_memory=False
+    )
 
-print(df.head())
-print(df["Address"].head())
-print(df["County"].head())
-print(df["Eircode"].head())
+
+    return df
+
+df = read_csv()
+
+df = df.rename(columns={
+    "Date of Sale (dd/mm/yyyy)": "date",
+    "Address": "address",
+    "County": "county",
+    "Eircode": "eircode",
+    "Price (€)": "price",
+    "Not Full Market Price": "not_full_market_price",
+    "VAT Exclusive": "vat_exclusive",
+    "Description of Property": "property_type",
+    "Property Size Description": "property_size"
+})
+
+print(df["county"].head())
+print(df["eircode"].head())
+print(df["price"].head())
